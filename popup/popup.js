ce49c92488f5db625157e07b2823c0091a13d4ba
ray176 <ray176@me.com>
2012-08-15 12:52:50 +0200

    First prototype for popup
    and some empty files so the extension is installable

---
+++ popup/popup.js
@@ -0,0 +1,18 @@
+/**
+ * Initialize POPUP namespace
+ */
+var POPUP = {};
+
+POPUP.Search = function() {this._init()}
+POPUP.Search.prototype = {
+
+	_init: function() {
+		
+	}
+
+}
+
+// initialize popup javascript
+$(function() {
+	CORE.initPage();
+});
